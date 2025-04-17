78828b247e07e416eaed1f6fe2b617d30de93807
raru48486 <raru48486@gmail.com>
2025-04-17 23:05:55 +0900

    viteでビルドできるようにした。

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+const libraryName = 'changetrackableproxy';
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'lib/main.js'),
+            name: libraryName,
+            fileName: (format) => `${libraryName}.${format}.js`,
+        },
+        sourcemap:true,
+    },
+});
